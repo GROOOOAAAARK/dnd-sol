af73694b595e972f4d77cbc582f22cd1fee46100
GROOOOAAAARK <t01_n10@protonmail.com>
2025-04-18 15:38:16 +0200

    🐛 fix initialization accounts

--- programs/dnd_sol/src/lib.rs
+++ programs/dnd_sol/src/lib.rs
@@ -13,10 +13,32 @@ pub mod dnd_sol {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+pub struct Initialize<'info> {
+    #[account(init, payer=owner, space = 8+Character::SPACE, seeds = [b"dnd_sol"], bump)]
+    pub character: Account<'info, Character>,
+
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(init, payer=payer, space = 8+Character::SPACE, seeds = [b"dnd_sol"], bump)]
+    pub character: Account<'info, CharacterAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(init, payer=payer, space = 8+Character::SPACE, seeds = [b"dnd_sol"], bump)]
+    pub character: Account<'info, CharacterAccount>,
+
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
-pub struct CreateCharacter<'info> {
+pub struct CharacterScope<'info> {
     #[account(
         init,
         payer = player,
