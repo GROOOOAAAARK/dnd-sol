10ff2521a51508760fa1dac4b755edefb61481dd
GRK <gitkey.aluminum361@passmail.net>
2025-10-11 17:30:07 +0200

    ✨ implement api to get first or specific step of an adventure

---
+++ src/app/api/adventures/[adventureId]/steps/[stepId]/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server"
+import { mockAdventures, mockGameSteps } from "@/assets/mocks/adventures"
+
+export async function GET(request: Request, { params }: { params: { adventureId: string, stepId: string } }) {
+  const adventureId = params.adventureId
+  const stepId = params.stepId
+
+  // Simulate network delay
+  await new Promise((resolve) => setTimeout(resolve, 100))
+
+  if (stepId === "first") {
+    const adventure = mockAdventures.find((adv) => adv.id === adventureId)
+    if (!adventure) {
+      return NextResponse.json({ error: "Adventure not found" }, { status: 404 })
+    }
+    return NextResponse.json(mockGameSteps[adventure.first_step_id])
+  }
+
+  const gameStep = mockGameSteps[stepId]
+
+  if (!gameStep) {
+    return NextResponse.json({ error: "Game step not found" }, { status: 404 })
+  }
+
+  return NextResponse.json(gameStep)
+}
